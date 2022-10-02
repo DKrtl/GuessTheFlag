88b6705d5a4423f91622ad07737bde247e3b0c2b
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-10-02 16:17:43 +0100

    create outline of layout

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Monaco", "Nigeria", "Poland", "Russia", "Spain", "UK", "USA"]
+    private var randomCountry = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.indigo
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                VStack {
+                    Text("Which flag belongs to")
+                    Text("\(countries[randomCountry])")
+                }
+                .foregroundColor(.white)
+            
+                ForEach(0..<3) { i in
+                    Button {
+                        
+                    } label: {
+                        Image(countries[i].lowercased())
+                            .renderingMode(.original)
+                    }
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
