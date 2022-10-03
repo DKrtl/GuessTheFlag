545922d07833bd36877de58ea4b41d8aea5e4aad
Dogukan Kartal <dogukan_kartal98@hotmail.com>
2022-10-03 17:43:19 +0100

    modify layout by adding a title and score + change the background

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -15,29 +15,54 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.indigo
-                .ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.796, green: 0.224, blue: 0.102), location: 0.3),
+                .init(color: Color(red: 0.161, green: 0.29, blue: 0.804), location: 0.3)
+            ], center: .top, startRadius: 200, endRadius: 500)
+            .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Which flag belongs to")
-                        .font(.subheadline.weight(.heavy))
-                    Text("\(countries[randomCountry])")
-                        .font(.largeTitle.weight(.semibold))
-                }
-                .foregroundColor(.white)
-            
-                ForEach(0..<3) { i in
-                    Button {
-                        resultIsPresented = true
-                        flagTapped(i)
-                    } label: {
-                        Image(countries[i].lowercased())
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 5)
+            VStack {
+                Spacer()
+                
+                Text("Guess The Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundColor(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Which flag belongs to")
+                            .font(.subheadline.weight(.heavy))
+                            .foregroundStyle(.secondary)
+                        
+                        Text("\(countries[randomCountry])")
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3) { i in
+                        Button {
+                            resultIsPresented = true
+                            flagTapped(i)
+                        } label: {
+                            Image(countries[i].lowercased())
+                                .renderingMode(.original)
+                                .clipShape(Capsule())
+                                .shadow(radius: 5)
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background(.regularMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
             .padding()
         }
